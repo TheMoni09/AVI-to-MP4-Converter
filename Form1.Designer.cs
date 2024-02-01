da3d323412f38aef9ce2e4f585102cd9349ac3d6
Simeon Petkov <spetkov273@gmail.com>
2024-02-01 23:09:49 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.ComponentModel;
+using NReco.VideoConverter;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AVI_to_MP4_Converter
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Input File";
+            this.openFileDialog1.Filter = "AVI Files (.avi)|*.avi";
+            this.openFileDialog1.InitialDirectory = "C:\\";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(330, 143);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(125, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Set Filename to Date";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(339, 166);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(350, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(334, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File to Convert";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(358, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AVI_to_MP4_Converter
+{
+    public partial class Form1 : Form
+    {
+        private string path = "";
+        private bool outputDate = false;
+        private string outputFile = "";
+        private string outputPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)}\\Converter Output";
+        private Stopwatch stopwatch;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Start a background thread for the conversion
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += OnConvert;
+            worker.RunWorkerCompleted += ConversionCompleted;
+
+            // Disable UI controls during conversion
+            button1.Enabled = false;
+            button2.Enabled = false;
+            checkBox1.Enabled = false;
+            textBox1.Enabled = false;
+
+            stopwatch = Stopwatch.StartNew();
+
+            worker.RunWorkerAsync();
+        }
+
+        private void ConversionCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            System.Media.SystemSounds.Exclamation.Play();
+
+            stopwatch.Stop();
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            checkBox1.Enabled = true;
+            textBox1.Enabled = true;
+
+            MessageBox.Show($"Conversion Finished! Took: {stopwatch.Elapsed.Hours:D2}:{stopwatch.Elapsed.Minutes:D2}:{stopwatch.Elapsed.Seconds:D2}", "Converter");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OnBrowseInput(sender, e);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            OnCheckboxValueChanged(sender, e);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            OnUserOpenedInput(sender, e);
+        }
+
+        private void OnConvert(object sender, EventArgs e)
+        {
+            var converter = new FFMpegConverter();
+
+            if (outputDate)
+            {
+                FileInfo fileInfo = new FileInfo(path);
+                string formattedCreationDate = fileInfo.CreationTime.ToString("yyyy-MM-dd");
+                outputFile = formattedCreationDate;
+            }
+            else
+            {
+                outputFile = this.textBox1.Text;
+            }
+
+            if(!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+
+            converter.ConvertMedia(path, $"{outputPath}\\{outputFile}.mp4", "mp4");
+        }
+
+        private void OnCheckboxValueChanged(object sender, EventArgs e)
+        {
+            CheckBox checkbox = (CheckBox)sender;
+            if (checkbox.Checked)
+            {
+                this.textBox1.Enabled = true;
+            }
+            else this.textBox1.Enabled = false;
+
+            outputDate = checkbox.Checked;
+        }
+
+        private void OnUserOpenedInput(object sender, CancelEventArgs e)
+        {
+            OpenFileDialog dialog = (OpenFileDialog)sender;
+            path = dialog.FileName;
+        }
+
+        private void OnBrowseInput(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(this);
+        }
+    }
+}
