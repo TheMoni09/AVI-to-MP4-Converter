815b60ffc2ac63a55968c270cae062a1d2a73ce6
Simeon Petkov <spetkov273@gmail.com>
2024-02-02 13:56:24 +0200

    Fixed bugs!

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace AVI_to_MP4_Converter
@@ -40,16 +41,23 @@ namespace AVI_to_MP4_Converter
 
         private void ConversionCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            stopwatch.Stop();
+
             System.Media.SystemSounds.Exclamation.Play();
 
-            stopwatch.Stop();
+            string outputPathResult = e.Result as string;
 
             button1.Enabled = true;
             button2.Enabled = true;
             checkBox1.Enabled = true;
-            textBox1.Enabled = true;
+            
+            if(!outputDate) textBox1.Enabled = true;
 
-            MessageBox.Show($"Conversion Finished! Took: {stopwatch.Elapsed.Hours:D2}:{stopwatch.Elapsed.Minutes:D2}:{stopwatch.Elapsed.Seconds:D2}", "Converter");
+            StringBuilder message = new StringBuilder();
+            message.AppendLine($"Conversion Finished! Took: {stopwatch.Elapsed.Hours:D2}:{stopwatch.Elapsed.Minutes:D2}:{stopwatch.Elapsed.Seconds:D2}");
+            message.AppendLine($"Output Path: {outputPathResult}");
+
+            DialogResult result = MessageBox.Show(message.ToString(), "Converter", MessageBoxButtons.OKCancel);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -67,19 +75,24 @@ namespace AVI_to_MP4_Converter
             OnUserOpenedInput(sender, e);
         }
 
-        private void OnConvert(object sender, EventArgs e)
+        private void OnConvert(object sender, DoWorkEventArgs e)
         {
             var converter = new FFMpegConverter();
+            FileInfo fileInfo = new FileInfo(path);
+
 
             if (outputDate)
             {
-                FileInfo fileInfo = new FileInfo(path);
                 string formattedCreationDate = fileInfo.CreationTime.ToString("yyyy-MM-dd");
                 outputFile = formattedCreationDate;
             }
             else
             {
-                outputFile = this.textBox1.Text;
+                if (String.IsNullOrEmpty(textBox1.Text))
+                {
+                    outputFile = fileInfo.Name;
+                }
+                else outputFile = textBox1.Text;
             }
 
             if(!Directory.Exists(outputPath))
@@ -87,7 +100,11 @@ namespace AVI_to_MP4_Converter
                 Directory.CreateDirectory(outputPath);
             }
 
-            converter.ConvertMedia(path, $"{outputPath}\\{outputFile}.mp4", "mp4");
+            string outputPathToFile = $"{outputPath}\\{outputFile}.mp4";
+
+            converter.ConvertMedia(path, outputPathToFile, "mp4");
+
+            e.Result = outputPathToFile;
         }
 
         private void OnCheckboxValueChanged(object sender, EventArgs e)
